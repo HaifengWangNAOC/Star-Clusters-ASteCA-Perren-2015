546fd3a6132c03a3290be1daae898baf995b0860
Gabriel-p <gabrielperren@gmail.com>
2014-12-02 15:38:46 -0300

    1 decont_algor_bys use global vars

--- functions/decont_algor_bys.py
+++ functions/decont_algor_bys.py
@@ -5,6 +5,40 @@
 import numpy as np
 
 
+def mpas(cl_reg_rad, runs_fields_probs):
+    """
+    Append averaged probabilities to each star inside the cluster radius and
+    sort by their values.
+    """
+
+    # cl_reg_rad = [[id,x,y,mag,e_mag,color1,e_col1], [], [], ...]
+
+    # Average all Bayesian membership probabilities into a single value for
+    # each star inside 'cl_reg_rad'.
+    clust_reg_prob_avrg = np.asarray(runs_fields_probs).mean(1).mean(0)
+
+    # Create new list appending the membership probability to each star inside
+    # the cluster radius.
+    temp_prob_members = []
+    for st_indx, star in enumerate(cl_reg_rad):
+            temp_prob_members.append(star +
+            [round(clust_reg_prob_avrg[st_indx], 3)])
+
+    # Stars inside the cluster's radius are now saved in the list
+    # 'temp_prob_members' where each item contains the data for each
+    # star: [id,x,y,mag,e_mag,color1,e_col1,memb_prob].
+
+    # Sort this list first by the membership probability from max
+    # value (1) to min (0) and then by its error values and magnitude value,
+    # (in that order) from min to max value.
+    # item[7] is the star's memb_prob and item[3] its magnitude.
+    membership_prob_avrg_sort = sorted(temp_prob_members,
+                                       key=lambda item: (-item[7], item[4],
+                                                         item[6], item[3]))
+
+    return membership_prob_avrg_sort
+
+
 def likelihood(region, cl_reg_rad):
     '''
     Obtain the likelihood/membership probability for each star in the
@@ -55,40 +89,6 @@ def likelihood(region, cl_reg_rad):
         clust_stars_probs.append(max(likelihood, epsilon))
 
     return clust_stars_probs
-
-
-def mpas(cl_reg_rad, runs_fields_probs):
-    """
-    Append averaged probabilities to each star inside the cluster radius and
-    sort by their values.
-    """
-
-    # cl_reg_rad = [[id,x,y,mag,e_mag,color1,e_col1], [], [], ...]
-
-    # Average all Bayesian membership probabilities into a single value for
-    # each star inside 'cl_reg_rad'.
-    clust_reg_prob_avrg = np.asarray(runs_fields_probs).mean(1).mean(0)
-
-    # Create new list appending the membership probability to each star inside
-    # the cluster radius.
-    temp_prob_members = []
-    for st_indx, star in enumerate(cl_reg_rad):
-            temp_prob_members.append(star +
-            [round(clust_reg_prob_avrg[st_indx], 3)])
-
-    # Stars inside the cluster's radius are now saved in the list
-    # 'temp_prob_members' where each item contains the data for each
-    # star: [id,x,y,mag,e_mag,color1,e_col1,memb_prob].
-
-    # Sort this list first by the membership probability from max
-    # value (1) to min (0) and then by its error values and magnitude value,
-    # (in that order) from min to max value.
-    # item[7] is the star's memb_prob and item[3] its magnitude.
-    membership_prob_avrg_sort = sorted(temp_prob_members,
-                                       key=lambda item: (-item[7], item[4],
-                                                         item[6], item[3]))
-
-    return membership_prob_avrg_sort
 
 
 def bys_da(flag_area_stronger, cl_region, field_region, memb_file, da_params):
