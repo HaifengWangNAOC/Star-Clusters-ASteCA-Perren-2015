5e01faaa3310a426e36a7e69dd338a7e198e19a4
Gabriel-p <gabrielperren@gmail.com>
2014-12-02 14:04:06 -0300

    use global vars

--- functions/get_data.py
+++ functions/get_data.py
@@ -3,38 +3,70 @@
 """
 
 import numpy as np
+import get_in_params as g
 
 
-def rem_bad_stars(id_star, x_data, y_data, mag_data, e_mag, col1_data,
-        e_col1):
+def rem_bad_stars(id_star, x_data, y_data, mag_data, e_mag, col_data,
+        e_col):
     '''
     Remove stars from all lists that have too large magnitude or color
     values (or their errors) which indicates a bad photometry.
     '''
-    # Set value of maximum limit.
+    # Set photometric range for accepted stars.
     min_lim, max_lim = -50., 50.
 
     # Store indexes of stars that should be removed.
-    lists_arr = zip(mag_data, e_mag, col1_data, e_col1)
-    del_indexes = [i for i, t in enumerate(lists_arr) if
-        any(e > max_lim for e in t) or any(e < min_lim for e in t)]
 
-    # Remove stars from id list first since this are strings.
+    # For magnitudes.
+    m_del_ind = []
+    for i, mag in enumerate(mag_data):
+        lists_arr = zip(mag, e_mag[i])
+        m_del_ind.append([i for i, t in enumerate(lists_arr) if
+            any(e > max_lim for e in t) or any(e < min_lim for e in t)])
+    # For colors.
+    c_del_ind = []
+    for i, col in enumerate(col_data):
+        lists_arr = zip(col, e_col[i])
+        c_del_ind.append([i for i, t in enumerate(lists_arr) if
+            any(e > max_lim for e in t) or any(e < min_lim for e in t)])
+
+    # Combine magnitude and color indexes of lines to be removed.
+    del_ind = m_del_ind + c_del_ind
+    del_indexes = [_ for sublist in del_ind for _ in sublist]
+
+    # Remove stars from id list.
     id_clean = np.delete(np.array(id_star), del_indexes)
-    # Remove stars from the rest of the lists simultaneously.
-    clean_array = np.delete(np.array([x_data, y_data, mag_data, e_mag,
-        col1_data, e_col1]), del_indexes, axis=1)
 
-    return id_clean, clean_array
+    # Remove stars from the x, y coordinates.
+    coord_clean_arr = np.delete(np.array([x_data, y_data]), del_indexes, axis=1)
+    # Array of positions with ALL stars.
+    #coord_dirt_arr = np.array([x_data, y_data])
+
+    # Remove stars from the magnitude columns.
+    m_clean_arr, em_clean_arr = [], []
+    for i, mag in enumerate(mag_data):
+        m_clean_arr.append(np.delete(np.array(mag), del_indexes))
+        em_clean_arr.append(np.delete(np.array(e_mag[i]), del_indexes))
+
+    # Remove stars from the color columns.
+    c_clean_arr, ec_clean_arr = [], []
+    for i, col in enumerate(col_data):
+        c_clean_arr.append(np.delete(np.array(col), del_indexes))
+        ec_clean_arr.append(np.delete(np.array(e_col[i]), del_indexes))
+
+    return id_clean, coord_clean_arr, m_clean_arr, em_clean_arr, c_clean_arr,\
+    ec_clean_arr
 
 
-def get_data(data_file, gd_params):
+def get_data(data_file, phot_params):
     '''
-    Get photometric data from the cluster's data file.
+    Get spatial and photometric data from the cluster's data file.
     '''
 
     # Read indexes from input params.
-    id_inx, x_inx, y_inx, m_inx, em_inx, c_inx, ec_inx = gd_params[:-1]
+    id_inx, x_inx, y_inx = g.gd_params[0][:-1]
+    # Indexes.
+    magnitudes, colors, e_mags, e_cols = phot_params[0]
 
     # Loads the data in 'myfile' as a list of N lists where N is the number of
     # columns. Each of the N lists contains all the data for the column.
@@ -42,10 +74,12 @@ def get_data(data_file, gd_params):
     data = np.genfromtxt(data_file, dtype=float, filling_values=99.999,
                          unpack=True)
 
-    # Read data colums, except IDs.
-    x_data, y_data, mag_data, e_mag, col1_data, e_col1 = \
-    data[x_inx], data[y_inx], data[m_inx], data[em_inx],\
-    data[c_inx], data[ec_inx]
+    # Extract coordinates and photometric data colums, except IDs.
+    x_data, y_data = data[x_inx], data[y_inx]
+    mag_data = [data[i] for i in magnitudes]
+    e_mag = [data[i] for i in e_mags]
+    col_data = [data[i] for i in colors]
+    e_col = [data[i] for i in e_cols]
 
     # Now read IDs as strings.
     data = np.genfromtxt(data_file, dtype=str, unpack=True)
@@ -54,25 +88,15 @@ def get_data(data_file, gd_params):
 
     # If any mag or color value (or their errors) is too large, discard
     # that star.
-    id_star, [x_data, y_data, mag_data, e_mag, col1_data, e_col1] = \
-    rem_bad_stars(id_star, x_data, y_data, mag_data, e_mag, col1_data, e_col1)
-
-    data_names = ['x_coords', 'y_coords', 'magnitudes', 'color']
-    try:
-        for i, dat_lst in enumerate([x_data, y_data, mag_data, col1_data]):
-            if min(dat_lst) == max(dat_lst):
-                print ("  ERROR: the range defined for the column {}\n"
-                "  is badly defined. Halting. Check the input data"
-                "  format.").format(data_names[i])
-    except ValueError:
-        raise ValueError('Bad format for input data. Check input file.')
+    id_star, [x_data, y_data], mag_data, e_mag, col_data, e_col = \
+    rem_bad_stars(id_star, x_data, y_data, mag_data, e_mag, col_data, e_col)
 
     print 'Data obtained from input file (N_stars: %d).' % len(id_star)
     if (n_old - len(id_star)) > 0:
-        print '  Stars rejected: %d.' % (n_old - len(id_star))
+        print ' Entries rejected: %d.' % (n_old - len(id_star))
 
     if len(id_star) / n_old < 0.5:
         print '  WARNING: {:.0f}% of stars rejected.'.format(100. *
         (1. - float(len(id_star)) / float(n_old)))
 
-    return id_star, x_data, y_data, mag_data, e_mag, col1_data, e_col1
+    return [id_star, x_data, y_data], [mag_data, e_mag, col_data, e_col]
